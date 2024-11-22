39131bab701f55af6062ccbfe4e850fd4dfff038
Ishu Bansal <ishubansal1400@gmail.com>
2024-11-22 20:57:07 +0530

    Match to the simulation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
-import Counter from './components/Counter';
 
 function App() {
-  return <Counter />;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div>
+        <div id="counter">{count}</div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+        <div id="message">{count > 10 ? "Threshold crossed!" : ""}</div>
+    </div>
+  );
 }
 
 export default App;
