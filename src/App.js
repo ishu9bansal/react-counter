75829f3861e5a308899d06840e7884bfcc13fff8
Ishu Bansal <ishubansal1400@gmail.com>
2024-11-27 20:01:18 +0530

    show filtered counters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
+const counterList = [
+  {id: 'A', name: "Counter A"},
+  {id: 'B', name: "Counter B"},
+  {id: 'C', name: "Counter C"},
+];
+
 function App() {
-  return <Counter/>;
+  const [showB, setShowB] = useState(true);
+  const handleToggle = () => setShowB(val => !val);
+  const filteredList = counterList.filter((value, ind) => {
+    return ind !== 1 || showB;
+  });
+  return <>
+    <button onClick={handleToggle}>Toggle Counter B visibility</button>
+    {filteredList.map(obj => <Counter name={obj.name} key={obj.id} />)}
+  </>;
 }
 
 export default App;
