0f1b3b060c11de826600f55df93995c09f1840db
Ishu Bansal <ishubansal1400@gmail.com>
2024-11-27 19:53:38 +0530

    toggle counters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
+const counterList = [
+  {id: 'A', name: "Counter A"},
+  {id: 'B', name: "Counter B"},
+  {id: 'C', name: "Counter C"},
+];
+
 function App() {
-  return <Counter/>;
+  const [toggle, setToggle] = useState(0);
+  const listLength = counterList.length;
+  const handleToggle = () => setToggle(toggle => (toggle + 1)%listLength);
+  const obj = counterList[toggle];
+  return <>
+    <button onClick={handleToggle}>Toggle Counters</button>
+    <Counter name={obj.name} key={obj.id} />
+  </>;
 }
 
 export default App;
