4c2fcee3923dfb9c353a581b3a70872e27bca688
Ishu Bansal <ishubansal1400@gmail.com>
2024-11-29 13:03:46 +0530

    insert a backend for counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import './Counter.css';
+import backend from '../services/backend';
 
 function Counter({ name }) {
   const [count, setCount] = useState(0);
+  const isFirstRender = useRef(true);
 
   useEffect(() => {
-    console.log("Counter created ", name);
+    backend.connect(name).then((prevCount) => setCount(prevCount));
 
-    return () => console.log("Counter destroyed", name);
+    return () => backend.disconnect(name);
   }, []);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    backend.updateMap(count);
+  }, [count]);
 
   const increment = () => setCount(count + 1);
   const reset = () => setCount(0);
